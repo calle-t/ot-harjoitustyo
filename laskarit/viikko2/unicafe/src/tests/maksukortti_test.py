7e0cba435040c90bc5e46bdff0d79bc4ad1ee62b
Calle <calle.thurin@welho.com>
2023-03-28 21:05:51 +0300

    Tehtävä 6 testit

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,23 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    # omat testit, tehtävä 6
+
+    def test_kortin_saldo_alussa_oikein(self):
+        self.assertEqual(self.maksukortti.saldo, 1000)
+
+    def test_rahan_lataaminen_kasvattaa_saldoa_oikein(self):
+        self.maksukortti.lataa_rahaa(100)
+
+        self.assertEqual(self.maksukortti.saldo, 1100)
+
+    def test_rahan_ottaminen(self):
+        self.maksukortti.ota_rahaa(100)
+        self.assertEqual(self.maksukortti.saldo, 900)
+
+        self.maksukortti.ota_rahaa(1000)
+        self.assertEqual(self.maksukortti.saldo, 900)
+
+        self.assertEqual(self.maksukortti.ota_rahaa(1000), False)
+        self.assertEqual(self.maksukortti.ota_rahaa(100), True)
